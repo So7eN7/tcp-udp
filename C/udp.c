7810bd4d2a52d8d3f83213f4e816a3557c934eef
soren <soorenaaryan7@protonmail.com>
2026-07-23 18:35:18 +0330

    IP packet

--- C/udp.c
+++ C/udp.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <arpa/inet.h>
 #include <unistd.h>
+
+unsigned short checksum(void *b, int len) {
+  unsigned short *buf = b;
+  unsigned int sum = 0;
+  while (len > 1) {
+    sum += *buf++;
+    len -= 2;
+  }
+  if (len == 1) sum += *(unsigned char *)buf;
+  sum = (sum >> 16) + (sum & 0xffff);
+  sum += (sum >> 16);
+  return ~sum;
+}
 
 int main() {
   int sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
@@ -13,13 +28,37 @@ int main() {
   }
 
   int one = 1;
-  if (setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0) {
-    perror("Setsockopt failed");
-    close(sock);
-    exit(1);
-  }
+  setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one));
 
-  printf("Raw socket set up\n");
+  struct sockaddr_in dest;
+  memset(&dest, 0, sizeof(dest));
+  dest.sin_family = AF_INET;
+  inet_pton(AF_INET, "127.0.0.1", &dest.sin_addr);
+
+  char packet[4096];
+  struct iphdr *iph = (struct iphdr *)packet;
+  iph->ihl = 5;
+  iph->version = 4;
+  iph->tos = 0;
+  iph->tot_len = sizeof(struct iphdr) + 5;
+  iph->id = htonl(54321);
+  iph->frag_off = 0;
+  iph->ttl = 64;
+  iph->protocol = IPPROTO_UDP;
+  iph->check = 0;
+  iph->saddr = inet_addr("127.0.0.1");
+  iph->daddr = dest.sin_addr.s_addr;
+
+  strcpy(packet + sizeof(struct iphdr), "hello");
+
+  iph->check = checksum((unsigned short *)packet, iph->tot_len);
+
+  if (sendto(sock, packet, iph->tot_len, 0, (struct sockaddr *)&dest, sizeof(dest)) < 0) {
+      perror("Sendto");
+      exit(1);
+    }
+
+  printf("IP packet sent\n");
   close(sock);
   return 0;
 }
--- python/udp.py
+++ python/udp.py
@@ -1,12 +1,47 @@
 import socket
+import struct
 import sys
+
+def checksum(data):
+    s = 0
+    for i in range(0, len(data), 2):
+        w = (data[i] << 8) + (data[i+1] if i+1 < len(data) else 0)
+        s += w 
+    s = (s >> 16) + (s & 0xffff)
+    s += s >> 16
+    return ~s & 0xffff 
 
 try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-    print("Raw socket set up")
+
+    dest_ip = '127.0.0.1'
+    src_ip = '127.0.0.1'
+    ip_header = struct.pack('!BBHHHBBH4s4s', #network byte order
+                            4 << 4 | 5, #version | ihl 
+                            0, # tos
+                            20 + 5, # total length (header + "hello")
+                            54321, # id 
+                            0, # frag_off
+                            64, # ttl 
+                            socket.IPPROTO_UDP, # protocol placeholder
+                            0, # checksum placeholder
+                            socket.inet_aton(src_ip),
+                            socket.inet_aton(dest_ip))
+    data = b'hello'
+    packet = ip_header + data
+
+    chksum = checksum(packet)
+    ip_header = struct.pack('!BBHHHBBH4s4s',
+                            4 << 4 | 5, 0, 20+5, 54321, 0, 64,
+                            socket.IPPROTO_UDP, chksum, 
+                            socket.inet_aton(src_ip), socket.inet_aton(dest_ip))
+    packet = ip_header + data
+
+    sock.sendto(packet, (dest_ip, 0))
+    print("IP packet sent")
 except socket.error as err:
-    print(f"Socket creation failed: {err}")
+    print(f"error: {err}")
     sys.exit(1)
 finally:
     sock.close()
